e36ab4ddffd0c4d05b6dba1a067d51e4435bc756
Joshua Martin <the.manuscriptmaster@gmail.com>
2022-02-10 10:53:18 -0500

    Add autoformatting

--- src/form-input.ts
+++ src/form-input.ts
@@ -1,5 +1,19 @@
+import Cleave from 'cleave.js';
+import 'cleave.js/dist/addons/cleave-phone.us.js';
 import { css, html, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, query } from 'lit/decorators.js';
+
+const CLEAVE_OPTIONS: Record<HTMLInputElement['type'], object> = {
+  date: {
+    date: true,
+    datePattern: ['m', 'd', 'Y'],
+  },
+  tel: {
+    phone: true,
+    phoneRegionCode: 'US',
+    delimiter: '-',
+  },
+};
 
 @customElement('form-input')
 export class FormInput extends LitElement {
@@ -8,6 +22,10 @@ export class FormInput extends LitElement {
   @property() type: HTMLInputElement['type'] = 'text';
 
   @property({ type: String }) value = '';
+
+  @query('input') input!: HTMLInputElement | null;
+
+  @property({ attribute: false }) cleave!: Cleave | null;
 
   static styles = css`
     *,
@@ -31,20 +49,56 @@ export class FormInput extends LitElement {
     }
   `;
 
+  get _type() {
+    return this.type === 'date' ? 'text' : this.type;
+  }
+
   handleInput(event: Event) {
-    event.stopPropagation();
     this.dispatchEvent(
       // @ts-ignore
       new CustomEvent('input', { detail: event.target.value })
     );
   }
 
+  setupAutoformat() {
+    const OPTIONS = CLEAVE_OPTIONS[this.type];
+    if (OPTIONS) {
+      this.cleave = new Cleave(this.input as HTMLInputElement, {
+        ...OPTIONS,
+        onValueChanged: this.handleInput.bind(this),
+      });
+    } else {
+      this.input?.addEventListener('input', this.handleInput.bind(this));
+    }
+  }
+
+  teardownAutoformat() {
+    this.cleave?.destroy();
+    this.cleave = null;
+    this.input?.removeEventListener('input', this.handleInput.bind(this));
+  }
+
+  protected updated(
+    changedProperties: Map<string | number | symbol, unknown>
+  ): void {
+    if (changedProperties.has('type')) {
+      this.teardownAutoformat();
+      this.setupAutoformat();
+    }
+  }
+
+  disconnectedCallback(): void {
+    this.teardownAutoformat();
+  }
+
   render() {
     return html`<input
-      .type="${this.type}"
+      .type="${this._type}"
       .value="${this.value}"
       placeholder="${this.label}"
-      @input="${this.handleInput}"
+      @input="${(event: Event) => {
+        event.stopPropagation();
+      }}"
     />`;
   }
 }
